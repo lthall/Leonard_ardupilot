8f9d74fee42ddec83a999e9e0c12dbf1e3d43c9c
Andrew Tridgell <andrew@tridgell.net>
2019-10-12 20:28:24 +1100

    Copter: V4.3.0-beta3-HQ1

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.3.0-beta3"
+#define THISFIRMWARE "ArduCopter V4.3.0-beta3-HQ1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,3,0,FIRMWARE_VERSION_TYPE_BETA+3
