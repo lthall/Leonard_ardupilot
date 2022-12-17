b532278c0886a6951d27f3140a3d084ec2ab523a
Leonard Hall <leonardthall@gmail.com>
2022-12-17 17:26:26 +1030

    Version: V4.3.2-Cal1

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.3.2"
+#define THISFIRMWARE "ArduCopter V4.3.2-Cal1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,3,2,FIRMWARE_VERSION_TYPE_OFFICIAL
