d85840ff0eb846aa6d27a383466ea9214c0672bb
Leonard Hall <leonardthall@gmail.com>
2023-07-06 23:08:04 +0930

    AP_Gripper: Add delay before grab

--- libraries/AP_Gripper/AP_Gripper_Servo.cpp
+++ libraries/AP_Gripper/AP_Gripper_Servo.cpp
@@ -37,7 +37,9 @@ void AP_Gripper_Servo::grab()
     config.state = AP_Gripper::STATE_GRABBING;
 
     // move the servo to the grab position
-    SRV_Channels::set_output_pwm(SRV_Channel::k_gripper, config.grab_pwm);
+    if (!is_positive(config.delaygrab_time)) {
+        SRV_Channels::set_output_pwm(SRV_Channel::k_gripper, config.grab_pwm);
+    }
     _last_grab_or_release = AP_HAL::millis();
     gcs().send_text(MAV_SEVERITY_INFO, "Gripper load grabbing");
     AP::logger().Write_Event(LogEvent::GRIPPER_GRAB);
@@ -102,8 +104,15 @@ bool AP_Gripper_Servo::grabbed() const
 void AP_Gripper_Servo::update_gripper()
 {
     // Check for successful grabbed or released
-    if (config.state == AP_Gripper::STATE_GRABBING && has_state_pwm(config.grab_pwm)) {
-        config.state = AP_Gripper::STATE_GRABBED;
+    if (config.state == AP_Gripper::STATE_GRABBING) {
+        if (has_state_pwm(config.grab_pwm)) {
+            config.state = AP_Gripper::STATE_GRABBED;
+        } else {
+            // move the servo to the release position
+            if (is_positive(config.delaygrab_time) && (AP_HAL::millis() - _last_grab_or_release > (config.delaygrab_time * 1000.0))) {
+                SRV_Channels::set_output_pwm(SRV_Channel::k_gripper, config.grab_pwm);
+            }
+        }
     } else if (config.state == AP_Gripper::STATE_RELEASING && has_state_pwm(config.release_pwm)) {
         config.state = AP_Gripper::STATE_RELEASED;
     }
