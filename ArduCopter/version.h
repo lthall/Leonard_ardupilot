5b0cdc0bab468a5de72caeb6e43928a5bb557fdf
Leonard Hall <leonardthall@gmail.com>
2023-07-07 11:15:48 +0930

    Version: ArduCopter V4.3.7 AK

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.3.7"
+#define THISFIRMWARE "ArduCopter V4.3.7 AK"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,3,7,FIRMWARE_VERSION_TYPE_OFFICIAL
