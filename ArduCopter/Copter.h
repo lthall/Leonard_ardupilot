6275aaef173e1f8f132f12f6e1d4fce6b4c04ad6
Leonard Hall <leonardthall@gmail.com>
2023-07-10 00:56:17 +0930

    Copter: Add Ship Operations Mode

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -217,6 +217,7 @@ public:
     friend class ModeLoiter;
     friend class ModePosHold;
     friend class ModeRTL;
+    friend class ModeShipOperation;
     friend class ModeSmartRTL;
     friend class ModeSport;
     friend class ModeStabilize;
@@ -606,7 +607,8 @@ private:
         SMARTRTL           = 3,
         SMARTRTL_LAND      = 4,
         TERMINATE          = 5,
-        AUTO_DO_LAND_START = 6
+        AUTO_DO_LAND_START = 6,
+        SHIP_OPERATION     = 7
     };
 
     enum class FailsafeOption {
@@ -631,6 +633,7 @@ private:
                                                       (int8_t)FailsafeAction::RTL,
                                                       (int8_t)FailsafeAction::SMARTRTL_LAND,
                                                       (int8_t)FailsafeAction::SMARTRTL,
+                                                      (int8_t)FailsafeAction::SHIP_OPERATION,
                                                       (int8_t)FailsafeAction::NONE,
                                                       -1 // the priority list must end with a sentinel of -1
                                                      };
@@ -766,6 +769,7 @@ private:
     void gpsglitch_check();
     void failsafe_deadreckon_check();
     void set_mode_RTL_or_land_with_pause(ModeReason reason);
+    void set_mode_Ship_Op_or_RTL_or_land_with_pause(ModeReason reason);
     void set_mode_SmartRTL_or_RTL(ModeReason reason);
     void set_mode_SmartRTL_or_land_with_pause(ModeReason reason);
     void set_mode_auto_do_land_start_or_RTL(ModeReason reason);
@@ -1018,6 +1022,9 @@ private:
 #endif
 #if MODE_TURTLE_ENABLED == ENABLED
     ModeTurtle mode_turtle;
+#endif
+#if MODE_SHIP_OPS_ENABLED == ENABLED
+    ModeShipOperation mode_ship_ops;
 #endif
 
     // mode.cpp
