53cb2c08db8f79b0a95c704f49bdd06523fbe6ae
Leonard Hall <leonardthall@gmail.com>
2024-08-17 19:19:08 +0930

    Version: ArduRover V4.5.5 B1

--- Rover/version.h
+++ Rover/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduRover V4.5.5"
+#define THISFIRMWARE "ArduRover V4.5.5 B1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,5,FIRMWARE_VERSION_TYPE_OFFICIAL
