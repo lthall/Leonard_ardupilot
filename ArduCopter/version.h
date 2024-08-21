0c4819277668857cf8f85350674d140c57aece37
lthall <leonardthall@gmail.com>
2024-08-21 15:49:44 +1000

    Version: V4.4.4 Cal1S4

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.4.4 Cal1S3"
+#define THISFIRMWARE "ArduCopter V4.4.4 Cal1S4"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,4,4,FIRMWARE_VERSION_TYPE_OFFICIAL
