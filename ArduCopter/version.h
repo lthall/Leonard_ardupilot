76870b3ea51c20679f455fbbecefe6ddd588ca01
Leonard Hall <leonardthall@gmail.com>
2024-10-09 00:02:40 +1030

    ArduCopter V4.5.7-C2.5r3

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.5.7-beta1"
+#define THISFIRMWARE "ArduCopter V4.5.7-C2.5r3"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,7,FIRMWARE_VERSION_TYPE_BETA
